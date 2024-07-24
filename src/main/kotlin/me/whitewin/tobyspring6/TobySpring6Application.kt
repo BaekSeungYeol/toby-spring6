08bd76557415e329a4b6195140c8a0263b998595
baekseungyeol <beck33333@naver.com>
2024-07-24 23:43:04 +0900

    first commit

---
+++ src/main/kotlin/me/whitewin/tobyspring6/TobySpring6Application.kt
@@ -0,0 +1,11 @@
+package me.whitewin.tobyspring6
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TobySpring6Application
+
+fun main(args: Array<String>) {
+    runApplication<TobySpring6Application>(*args)
+}
---
+++ src/test/kotlin/me/whitewin/tobyspring6/TobySpring6ApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.whitewin.tobyspring6
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TobySpring6ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
