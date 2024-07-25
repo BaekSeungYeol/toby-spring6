d8302bde1c13b12a4c96ded725e7f1958e2d6434
baekseungyeol <beck33333@naver.com>
2024-07-25 23:05:53 +0900

    feat(PaymentService): prepare 로직

---
+++ src/main/kotlin/me/whitewin/tobyspring6/Payment.kt
@@ -0,0 +1,14 @@
+package me.whitewin.tobyspring6
+
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+data class Payment(
+    val orderId: Long,
+    val currency: String,
+    val foreignCurrencyAmount: BigDecimal,
+    val exRate: BigDecimal,
+    val convertedAmount: BigDecimal,
+    val validUntil: LocalDateTime
+)
+
--- src/main/kotlin/me/whitewin/tobyspring6/TobySpring6Application.kt
+++ src/main/kotlin/me/whitewin/tobyspring6/TobySpring6Application.kt
@@ -3,9 +3,9 @@ package me.whitewin.tobyspring6
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class TobySpring6Application
-
-fun main(args: Array<String>) {
-    runApplication<TobySpring6Application>(*args)
-}
+//@SpringBootApplication
+//class TobySpring6Application
+//
+//fun main(args: Array<String>) {
+//    runApplication<TobySpring6Application>(*args)
+//}
